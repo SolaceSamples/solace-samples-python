dc03df293fdd8607a3a677260d826746d2b28ccb
Tamimi <tamimi@solace.com>
2020-12-22 15:46:54 -0500

    init gauranteed publisher

---
+++ samples/gauranteed_publisher.py
@@ -0,0 +1,103 @@
+import os
+import platform
+import time
+import threading
+
+from solace.messaging.messaging_service import MessagingService, ReconnectionListener, ReconnectionAttemptListener, ServiceInterruptionListener, RetryStrategy, ServiceEvent
+from solace.messaging.publisher.persistent_message_publisher import PersistentMessagePublisher
+from solace.messaging.publisher.persistent_message_publisher import MessageDeliveryListener
+from solace.messaging.resources.topic import Topic
+
+if platform.uname().system == 'Windows': os.environ["PYTHONUNBUFFERED"] = "1" # Disable stdout buffer 
+
+lock = threading.Lock() # lock object that is not owned by any thread. Used for synchronization and counting the 
+
+TOPIC_PREFIX = "samples/hello"
+
+# Inner classes for error handling
+class ServiceEventHandler(ReconnectionListener, ReconnectionAttemptListener, ServiceInterruptionListener):
+    def on_reconnected(self, e: ServiceEvent):
+        print("\non_reconnected")
+        print(f"Error cause: {e.get_cause()}")
+        print(f"Message: {e.get_message()}")
+    
+    def on_reconnecting(self, e: "ServiceEvent"):
+        print("\non_reconnecting")
+        print(f"Error cause: {e.get_cause()}")
+        print(f"Message: {e.get_message()}")
+
+    def on_service_interrupted(self, e: "ServiceEvent"):
+        print("\non_service_interrupted")
+        print(f"Error cause: {e.get_cause()}")
+        print(f"Message: {e.get_message()}")
+
+class MessageDeliveryListenerImpl(MessageDeliveryListener):
+    def __init__(self):
+        self._delivery_count = 0
+
+    @property
+    def get_delivery_count(self):
+        return self._delivery_count
+
+    def on_delivery_receipt(self, delivery_receipt: 'DeliveryReceipt'):
+        with lock:
+            self._delivery_count += 1
+            # print(f"\tMessage: {delivery_receipt.message}\n"
+            print(f"\tIs persisted: {delivery_receipt.is_persisted}\n"
+                  f"\tTimestamp: {delivery_receipt.time_stamp}\n"
+                  f"\tException: {delivery_receipt.exception}\n")
+            if delivery_receipt.user_context:
+                print(f'\tUsercontext received: {delivery_receipt.user_context.get_custom_message}')
+
+# Broker Config. Note: Could pass other properties Look into
+broker_props = {
+    "solace.messaging.transport.host": os.environ.get('SOLACE_HOST') or "localhost",
+    "solace.messaging.service.vpn-name": os.environ.get('SOLACE_VPN') or "default",
+    "solace.messaging.authentication.scheme.basic.username": os.environ.get('SOLACE_USERNAME') or "default",
+    "solace.messaging.authentication.scheme.basic.password": os.environ.get('SOLACE_PASSWORD') or "default"
+    }
+
+# Build A messaging service with a reconnection strategy of 20 retries over an interval of 3 seconds
+# Note: The reconnections strategy could also be configured using the broker properties object
+messaging_service = MessagingService.builder().from_properties(broker_props)\
+                    .with_reconnection_retry_strategy(RetryStrategy.parametrized_retry(20,3))\
+                    .build()
+
+# Blocking connect thread
+messaging_service.connect()
+print(f'Messaging Service connected? {messaging_service.is_connected}')
+
+# Event Handeling for the messaging service
+service_handler = ServiceEventHandler()
+
+# Create a persistent message publisher and start it
+publisher: PersistentMessagePublisher = messaging_service.create_persistent_message_publisher_builder().build()
+publisher.start_async()
+
+# set a message delivery listener to the publisher
+delivery_listener = MessageDeliveryListenerImpl()
+publisher.set_message_delivery_listener(delivery_listener)
+
+# Prepare the destination topic
+topic = Topic.of(TOPIC_PREFIX)
+
+# Prepare outbound message payload and body
+message_body = "this is the body of the msg"
+outbound_msg_builder = messaging_service.message_builder() \
+                .with_application_message_id("sample_id") \
+                .with_property("application", "samples") \
+                .with_property("language", "Python")
+
+outbound_msg = outbound_msg_builder.build(message_body)
+try:
+    while True:
+        publisher.publish(outbound_msg, topic)
+        print(f'PERSISTENT publish message is successful... Topic: [{topic.get_name()}]')
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print(f'\nDelivery receipt count: {delivery_listener.get_delivery_count}\n')
+    print('\nTerminating Publisher')
+    publisher.terminate()
+    print('\nDisconnecting Messaging Service')
+    messaging_service.disconnect()
